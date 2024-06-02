412c8f098464f684cba3b73abfa9d33be5558f00
lany <work@lanyt.cc>
2024-06-02 16:28:04 +0800

    Create spotvm.rb

---
+++ spotvm.rb
@@ -0,0 +1,44 @@
+class Spotvm < Formula
+    desc "spot vm tool"
+    homepage "https://github.com/ysicing/spot"
+    version "0.3.1"
+
+    if OS.mac?
+      if Hardware::CPU.arm?
+        url "https://github.com/ysicing/spot/releases/download/v#{version}/spot_darwin_arm64"
+        sha256 "9923e9af28a982b279148df090433c668d080b1bac48325389ea542c5ed5bd8c"
+      else
+        url "https://github.com/ysicing/spot/releases/download/v#{version}/spot_darwin_amd64"
+        sha256 "8912347bcb1cb7743e142b95be7062eb39f722c8cf3e43cfc690ae57e5846b63"
+      end
+    elsif OS.linux?
+      if Hardware::CPU.intel?
+        url "https://github.com/ysicing/spot/releases/download/v#{version}/spot_linux_amd64"
+        sha256 "9fb58128d639311938632b30a58c639d4008afabc836b18c243e2f86f5702b81"
+      end
+      if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+        url "https://github.com/ysicing/spot/releases/download/v#{version}/spot_linux_arm64"
+        sha256 "cf69ddddf9839b80e684196cb25211544cae507533b06a174d370e845d1417e4"
+      end
+    end
+
+    def install
+      if OS.mac?
+        if Hardware::CPU.intel?
+          bin.install "spot_darwin_amd64" => "spotvm"
+        else
+          bin.install "spot_darwin_arm64" => "spotvm"
+        end
+      elsif OS.linux?
+        if Hardware::CPU.intel?
+          bin.install "spot_linux_amd64" => "spotvm"
+        else
+          bin.install "spot_linux_arm64" => "spotvm"
+        end
+      end
+    end
+
+    test do
+      assert_match "spotvm vervion v#{version}", shell_output("#{bin}/spotvm -v")
+    end
+end
